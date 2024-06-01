86d162375474513d02a0df48c4eb15de0557cff6
Mutai05 <kelvok2014@gmail.com>
2024-06-02 01:55:32 +0300

    Updated config.py file to store configuration variables for the application

---
+++ app/config.py
@@ -0,0 +1,10 @@
+# app/config.py
+
+import secrets
+
+class Config:
+    SECRET_KEY = secrets.token_hex(16)
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = ''
+    MYSQL_HOST = 'localhost'
+    MYSQL_DB = 'flaskdb'
