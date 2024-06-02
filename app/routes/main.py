651bfc9c49e476af7f67f8f3e0e9e0512b2cd5ed
Mutai05 <kelvok2014@gmail.com>
2024-06-02 23:59:57 +0300

    Added about page, faqs and contact page routes

--- app/routes/main.py
+++ app/routes/main.py
@@ -7,3 +7,15 @@ main_bp = Blueprint('main', __name__)
 @main_bp.route("/")
 def home():
     return render_template("main/index.html")
+
+@main_bp.route("/about-us")
+def about():
+    return render_template("main/about.html")
+
+@main_bp.route("/faqs")
+def faqs():
+    return render_template("main/faqs.html")
+
+@main_bp.route("/contact-us")
+def contact():
+    return render_template("main/contact.html")
