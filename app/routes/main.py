edd2d290827eae0c99da6f8254c603fd7fd7e233
Mutai05 <kelvok2014@gmail.com>
2024-06-07 18:08:08 +0300

    Updated main.py file to fetch job listings from db to render on index.html

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,12 +1,20 @@
 # app/routes/main.py
 
 from flask import Blueprint, render_template
+from ..models import get_db_connection
 
 main_bp = Blueprint('main', __name__)
 
 @main_bp.route("/")
 def home():
-    return render_template("main/index.html")
+    # Fetch job listings from database
+    cnx = get_db_connection()
+    cursor = cnx.cursor(dictionary=True)
+    cursor.execute("SELECT id, title, location, salary, currency FROM jobs")
+    jobs = cursor.fetchall()
+    cursor.close()
+    cnx.close()
+    return render_template("main/index.html", jobs=jobs)
 
 @main_bp.route("/about-us")
 def about():
