11d3a18e232a85248c540cd5f378381e315f6ab6
Mutai05 <kelvok2014@gmail.com>
2024-06-07 17:51:01 +0300

    Updated Flask blueprint that handles the routes related to job postings

--- app/routes/jobs.py
+++ app/routes/jobs.py
@@ -1,9 +1,19 @@
 # app/routes/jobs.py
 
 from flask import Blueprint, render_template
+from ..models import get_db_connection
 
 jobs_bp = Blueprint('jobs', __name__, url_prefix='/jobs')
 
 @jobs_bp.route("/")
 def jobs_archive():
-    return render_template("jobs/jobs_archive.html")
+    # Fetch job listings from database
+    cnx = get_db_connection()
+    cursor = cnx.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM jobs")
+    jobs = cursor.fetchall()
+    cursor.close()
+    cnx.close()
+
+    # Render the jobs archive page template with job listings data
+    return render_template("jobs/jobs_archive.html", jobs=jobs)
