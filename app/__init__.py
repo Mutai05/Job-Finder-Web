0d768f4b765ed46ff63e9e349017fee0f364e1ec
Mutai05 <kelvok2014@gmail.com>
2024-06-08 16:24:29 +0300

    Set static URL path

--- app/__init__.py
+++ app/__init__.py
@@ -7,11 +7,10 @@ from .routes.admin import admin_bp
 from .config import Config
 
 def jobfinder_web():
-    app = Flask(__name__)
+    app = Flask(__name__, static_url_path='/static')
     app.config.from_object(Config)
 
     # Register blueprints
-
     app.register_blueprint(main_bp)
     app.register_blueprint(jobs_bp)
     app.register_blueprint(admin_bp)
